1fe3503f4a07ccc2db39d21d5477b5b690e3d52a
Kenny Nguyen <sokenns@gmail.com>
2023-12-19 16:53:25 -0800

    Add header and page modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./style.css";
+import home from "./home";
+import menu from "./menu";
+import contact from "./contact";
+
+const body = document.querySelector("body");
+const content = document.querySelector("#content");
+const header = document.createElement("header");
+const nav = document.createElement("nav");
+const links = ["Home", "Menu", "Contact"];
+const pages = [home, menu, contact];
+
+for (let i = 0; i < links.length; i++) {
+  let link = document.createElement("h1");
+  link.textContent = links[i];
+  link.addEventListener("click", () => {
+    clearContent();
+    content.appendChild(pages[i]());
+  });
+  nav.appendChild(link);
+}
+
+header.appendChild(nav);
+body.insertBefore(header, content);
+
+// Set home as the default on page load
+content.appendChild(home());
+
+function clearContent() {
+  let module = document.querySelector(".module");
+  content.removeChild(module);
+}
